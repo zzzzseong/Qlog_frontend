e2225e768b8397d1b6246efd9323b51179f85923
zzzzseong <jung48182@gmail.com>
2023-06-28 18:47:04 +0900

    Update(Home): Profile 레이아웃 및 디자인 개선

--- src/pages/components/Profile.js
+++ src/pages/components/Profile.js
@@ -1,11 +1,28 @@
 const Profile = (props) => {
     return (
-        <div>
+        <div className="box">
             <img src={props.imgPath}></img>
-            <div>{props.name} : {props.point}pt</div>
-            <div>follower: 300 following: 300</div>
-            <div>Qcard: 150 solved: 142</div>
-            <button>Qcard 생성</button>
+            <div className="home-left-profile-info">
+                <div>
+                    <div className="profile-header">
+                        <div className="profile-header-title">
+                            <div className="profile-name">{props.name}</div>
+                            <div className="profile-point">{props.point}point</div>
+                        </div>
+                        {/* <div>follower: 300 following: 300</div> */}
+                    </div>
+                    <div className="profile-info-countBox">
+                        <div className="profile-countBox-left">
+                            <div>Qcard</div>
+                            <div>150</div>
+                        </div>
+                        <div className="profile-countBox-right">
+                            <div>solved</div>
+                            <div>200</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
