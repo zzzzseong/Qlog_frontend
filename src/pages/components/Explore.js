762dec3e3a02771b98e97120c211c5b95c54c039
zzzzseong <jung48182@gmail.com>
2023-07-03 22:09:30 +0900

    Update(Home): right container 버튼 이벤트 구현 완료

--- src/pages/components/Explore.js
+++ src/pages/components/Explore.js
@@ -1,7 +1,21 @@
-const Explore = () => {
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import CreateQCard from "./CreateQCard";
+
+
+const Explore = (props) => {
+    const handleButton = (e) => {
+        const createQCard = ReactDOM.createRoot(document.getElementsByClassName('home-right')[0]);
+        createQCard.render(React.createElement(CreateQCard, props, null));
+    };
+
     return (
         <div className="explore">
             <div className="explore-header">
+                <div className="explore-header-nav">
+                    <button onClick={handleButton}>New</button>
+                </div>
                 <h1>Let's solve</h1>
                 <h1>random question</h1>
             </div>
